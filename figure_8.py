48168fa7fc84d2399f500f76986b4a3ef8679375
Jason Furtney <jfurtney@itascacg.com>
2021-02-06 11:29:42 -0600

    clean up repo, add fractures from parameter study, fix problem in figure 8 plotting

--- figure_8.py
+++ figure_8.py
@@ -10,9 +10,9 @@ for i in range(5):
         if os.path.isfile(geom_file):
             x,y = np.loadtxt(geom_file, skiprows=2, usecols=(1,2)).T
             plt.subplot(5,5,k+1)
-            plt.scatter(x,y,marker=".", color="black")
+            plt.scatter(x,y,marker="o", color="black", s=0.25, edgecolor="black")
             plt.gca().set_aspect(1)
-            plt.ylim(-0.25,0.35)
+            plt.ylim(-0.35,0.35)
             plt.xlim(-0.35,0.35)
             plt.xticks([])
             plt.yticks([])
